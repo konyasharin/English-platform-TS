ee72a53986b05da9528e90aeb68a9c68f62fb3e1
Nikolay <kolechka.malyshev.04@gmail.com>
2023-12-18 22:27:12 +0700

    training learn was done

--- src/Trainings/TrainingPage.tsx
+++ src/Trainings/TrainingPage.tsx
@@ -1,14 +1,18 @@
 import Card from "./Card";
 import Btn from "../Btns/Btn";
-import LearnTraining from "../store/LearnTraining";
+import LearnTraining, {TrainingStatuses} from "../store/LearnTraining";
 import ModulesStore from "../store/ModulesStore";
 import {useNavigate} from "react-router-dom";
 import {useEffect} from "react";
 import {observer} from "mobx-react-lite";
-
+import Input from "../Input/Input";
+import InputClass from "../store/Input"
+import {InputNames} from "../initializeForms";
 
 const training = LearnTraining.getInstance() as LearnTraining
 let i: number = 0
+let j: number = 0
+const answersInput = new InputClass(InputNames.ANSWER, "", "Перевод")
 
 const TrainingPage = observer(() => {
   const navigate = useNavigate()
@@ -19,28 +23,56 @@ const TrainingPage = observer(() => {
       training.initializeTraining()
     }
   })
+  let secondBlock
+  let title
+  let firstBlock
+  switch (training.currentStatus){
+    case TrainingStatuses.SHOW_WORDS:
+      firstBlock = <Card text={training.currentWord ? training.currentWord.word : ""}/>
+      secondBlock = <Card text={training.currentWord ? training.currentWord.translate : ""}/>
+      title = <h2>Попытайтесь<br/>запомнить слова</h2>
+      break
+    case TrainingStatuses.WRITE_ANSWERS:
+      firstBlock = <Card text={training.currentWord ? training.currentWord.word : ""}/>
+      secondBlock = <Input placeholder={answersInput.placeholder} value={answersInput.text} edit={answersInput}/>
+      title = <h2>Попытайтесь ввести<br/>перевод слов</h2>
+      break
+    case TrainingStatuses.RESULTS:
+      firstBlock = <></>
+      secondBlock = <div>{training.correctAnswers} / {training.countOfWord}</div>
+      title = <h2>Результаты:</h2>
+  }
   return(
     <section className="container training-page">
-      <h2>Попытайтесь<br/>запомнить слова</h2>
+      {title}
       <div className="cards">
-        <Card text={training.currentWord ? training.currentWord.word : ""}/>
-        <Card text={training.currentWord ? training.currentWord.translate : ""}/>
+        {firstBlock}
+        {secondBlock}
       </div>
       {
         /*
-        Здесь важно передавать метод как стрелочную функцию, в противном случае не будет ссылки на
+        Здесь важно передавать метод nextWord как стрелочную функцию, в противном случае не будет ссылки на
         объект (this = undefined, связано это с тем, что если мы используем не стрелочную функцию, то
         передается сам метод класса без привязки к объекту)
          */
       }
       <Btn backgroundColor={"#4D4DFF"} text={"продолжить"} color={"#ffffff"} onClick={() => {
-        if(i < training.countOfWord - 1){
-          if(training.nextWord() === undefined){
-
+        if(training.currentStatus === TrainingStatuses.SHOW_WORDS){
+          if(i < training.countOfWord - 1){
+            training.nextWord()
+            i += 1
+          } else{
+            training.toggleToTrainingGroup()
           }
-          i += 1
-        } else{
-          alert("Кончились слова вообще")
+        } else if(training.currentStatus === TrainingStatuses.WRITE_ANSWERS){
+          if(j < training.countOfWord - 1){
+            training.checkAnswer(answersInput.text)
+            answersInput.onChange("")
+            training.nextWord()
+            j += 1
+          } else{
+            training.setFinishStatus()
+          }
         }
       }}/>
     </section>
--- src/store/LearnTraining.ts
+++ src/store/LearnTraining.ts
@@ -5,16 +5,24 @@ import Queue from "./Queue";
 import Word from "./Word";
 import Shuffler from "./Shuffler";
 
+export enum TrainingStatuses{
+  SHOW_WORDS = "SHOW_WORDS",
+  WRITE_ANSWERS = "WRITE_ANSWERS",
+  RESULTS = "RESULTS"
+}
+
 class LearnTraining extends Training{
   private _queue: Queue<Word>
   private _queueCurrentGroup: Queue<Word>
-  private _queueCurrentTrainingGroup: Queue<Word>
+  private _temp: Array<Word>
   private _currentWord: Word | undefined
   private _isInit: boolean
   private static _instance: LearnTraining
+  private _currentStatus: string
+  private _correctAnswers: number
   private constructor() {
     super();
-    makeObservable<this, "_countOfWord" | "_maxCountOfWord" | "_currentWord">(this, {
+    makeObservable<this, "_countOfWord" | "_maxCountOfWord" | "_currentWord" | "_currentStatus">(this, {
       _countOfWord: override,
       _maxCountOfWord: override,
       incrementCountOfWord: override,
@@ -23,12 +31,18 @@ class LearnTraining extends Training{
       countOfWord: override,
       _currentWord: observable,
       nextWord: action,
-      resetTraining: action
+      resetTraining: action,
+      _currentStatus: observable,
+      toggleStatus: action,
+      toggleToTrainingGroup: action,
+      setFinishStatus: action
     })
     this._queue = new Queue<Word>()
     this._queueCurrentGroup = new Queue<Word>()
-    this._queueCurrentTrainingGroup = new Queue<Word>()
+    this._temp = []
     this._isInit = false
+    this._currentStatus = TrainingStatuses.SHOW_WORDS
+    this._correctAnswers = 0
   }
 
   public static getInstance(){
@@ -63,10 +77,29 @@ class LearnTraining extends Training{
 
   public nextWord() {
     this._currentWord = this._queueCurrentGroup.dequeue()
-    if(this._currentWord !== undefined){
-      this._queueCurrentTrainingGroup.enqueue(this._currentWord)
+    if(this._currentWord !== undefined && this._currentStatus === TrainingStatuses.SHOW_WORDS){
+      this._temp.push(this._currentWord)
+    } else if(this._currentWord === undefined && this._currentStatus === TrainingStatuses.SHOW_WORDS){
+      this.toggleToTrainingGroup()
+    } else if(this._currentWord === undefined && this._currentStatus === TrainingStatuses.WRITE_ANSWERS){
+      this.nextGroup()
+      this._currentWord = this._queueCurrentGroup.dequeue()
+      this._temp.push(this._currentWord!)
+      this.toggleStatus()
     }
     return this._currentWord
+  }
+
+  public toggleToTrainingGroup(){
+    // перемешивание уже для ввода пользователем ответов
+    const shuffler = new Shuffler()
+    shuffler.shuffle<Word>(this._temp)
+    this._temp.forEach(word => {
+      this._queueCurrentGroup.enqueue(word)
+    })
+    this._temp = []
+    this.toggleStatus()
+    this.nextWord()
   }
 
   public get currentWord(){
@@ -88,6 +121,35 @@ class LearnTraining extends Training{
     this._queue = new Queue<Word>()
     this._queueCurrentGroup = new Queue<Word>()
     this._isInit = false
+  }
+
+  public get currentStatus(){
+    return this._currentStatus
+  }
+
+  public toggleStatus(){
+    switch (this._currentStatus){
+      case TrainingStatuses.SHOW_WORDS:
+        this._currentStatus = TrainingStatuses.WRITE_ANSWERS
+        break
+      case TrainingStatuses.WRITE_ANSWERS:
+        this._currentStatus = TrainingStatuses.SHOW_WORDS
+        break
+    }
+  }
+
+  public setFinishStatus(){
+    this._currentStatus = TrainingStatuses.RESULTS
+  }
+
+  public checkAnswer(answer: string){
+    if(this._currentWord?.translate === answer){
+      this._correctAnswers += 1
+    }
+  }
+
+  public get correctAnswers(){
+    return this._correctAnswers
   }
 }
 
